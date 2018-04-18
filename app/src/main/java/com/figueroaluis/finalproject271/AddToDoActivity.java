be20638888457ee6a68a632baed39fb6edae0608
Luis Figueroa <figueroa.ides@gmail.com>
2018-04-17 17:37:30 -0700

    remove app bar, fix bugs

---
+++ app/src/main/java/com/figueroaluis/finalproject271/AddToDoActivity.java
@@ -0,0 +1,19 @@
+package com.figueroaluis.finalproject271;
+
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+/**
+ * Created by luisfigueroa on 4/17/18.
+ */
+
+public class AddToDoActivity extends AppCompatActivity {
+    private ImageButton cancel_new_task_button;
+    private TextView add_task_title;
+    private EditText add_task_title_input;
+    private TextView add_task_date_title;
+    private EditText add_task_date_input;
+
+}
--- app/src/main/java/com/figueroaluis/finalproject271/MainActivity.java
+++ app/src/main/java/com/figueroaluis/finalproject271/MainActivity.java
@@ -1,13 +1,47 @@
 package com.figueroaluis.finalproject271;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Context mContext;
+    private Activity mActivity;
+    private RelativeLayout addTaskLayout;
+    private DrawerLayout mDrawerLayout;
+    private FloatingActionButton add_to_do_button;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // set mContext
+        mContext = getApplicationContext();
+        // get the main activity
+        mActivity = MainActivity.this;
+
+        // create the adapter
+        mDrawerLayout = findViewById(R.id.main_layout);
+        add_to_do_button =  findViewById(R.id.add_to_do_button);
+
+
+        // first set on click listener to open the second activity
+        add_to_do_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), AddToDoActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
